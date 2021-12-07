140789a65da4bb0b67fe39feee66d674a2d800cc
mohitrathor98 <mohitrathor8@gmail.com>
2021-12-07 12:56:55 +0530

    car creation

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,21 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_len = 4)
+        self.color(random.choice(COLORS))
+        self.penup()
+        self.place_cars()
+
+    def place_cars(self):
+        random_y = random.randrange(-280, 280)
+        self.goto(280, random_y)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import time
 from turtle import Screen
 from player import Player
@@ -8,7 +9,36 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+'''
+TODO:
+
+1) Create random coloured cars at random locations == done
+2) Move cars from right to left
+3) Create a turtle and move it upward
+4) Detect turtle's collision with top
+    --> Turtle goes back to original position
+    --> player levels up
+    --> car's speed increases
+5) Detect turtle's collision with cars
+    --> Everything stops
+    --> Game over flashes
+    --> exit on click
+'''
+
+
+# car creation
+car = CarManager()
+car_list = []
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    
+    # generate random number of cars
+    for car in range(random.randint(1, 3) + 1):
+        car_list.append(CarManager())
+    
+
+
+screen.exitonclick()
